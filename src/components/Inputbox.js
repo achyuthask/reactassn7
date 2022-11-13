b3f7af8cc8bde0e870f3ce8e7b5ecc1cf97cbc99
achyuthask <96536973+achyuthask@users.noreply.github.com>
2022-11-13 21:03:50 +0530

    first origin

---
+++ src/components/Inputbox.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Inputbox = (props) =>{
+    return(
+<textarea rows={10} cols={50} onChange ={(e)=>{props.setdatafn(e.target.value)}}></textarea>
+    )
+}
+
+export default Inputbox;
---
+++ src/components/screen.js
@@ -0,0 +1,16 @@
+import ReactMarkdown from 'react-markdown';
+import remarkGfm from 'remark-gfm';
+
+
+const Screen = (props) =>{
+    const markdown = props.markupdata
+
+  return <div>screen
+
+    <ReactMarkdown children={markdown} remarkPlugins={[remarkGfm]} />
+  </div>
+
+}
+
+
+export default Screen;
